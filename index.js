ac12130397fdbc45aa13a145db3bee3232eb4796
10619 <10619@localhost.com>
2015-08-19 11:45:10 +0200

    Basic Auth

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 'use strict';
 
 var express = require('express');
+var dbSetup = require('./lib/db-setup');
 var app = express();
+dbSetup.init();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(require('./lib/middlewares/session-setup'));
+app.use(require('./lib/middlewares/basic-auth'));
 app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), function () {
